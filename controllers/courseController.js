11f32b5b4148ae1c534c0d58ad91745a15aef173
Kapil Bhise <bhisekapil38@gmail.com>
2023-04-08 23:18:31 +0530

    changed errorHandler.js name

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -2,8 +2,9 @@ import cloudinary from "cloudinary";
 import { catchAsyncError } from "../middlewares/catchAsyncError.js";
 import { Course } from "../models/Course.js";
 import getDataUri from "../utils/dataURI.js";
-import ErrorHandler from "../utils/ErrorHandler.js";
 
+//errorHandler
+import ErrorHandler from "../utils/ErrorHandler.js";
 
 // get all courses without lectures
 export const getAllCourses = catchAsyncError(async (req, res, next) => {
